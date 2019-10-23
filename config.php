4e01453345df06e09d1da745dff2517e191a4d36
Muhammad Risky <56730687+riskyfreak@users.noreply.github.com>
2019-10-23 14:40:53 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?
 
 $UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0";
-$PHPSESSID = "PHPSESSID=8h7p29pektgambbl3kjrpsjlb3";
+$PHPSESSID = "PHPSESSID=03luf0haj9g0m8etv2l0upjqq2";
